2a2a86e25dc630b44f70b815fe061d6276d68803
AngadSingh04 <singh04angad@gmail.com>
2025-03-12 20:46:37 +0530

    Refactor AWS EC2 configuration to use dynamic AMI retrieval and add region data source

--- LAB7/main.tf
+++ LAB7/main.tf
@@ -4,7 +4,9 @@ provider "aws" {
 
 #Retrieve the list of AZs in the current AWS region
 data "aws_availability_zones" "available" {}
-data "aws_region" "current" {}
+data "aws_region" "MyRegion" {
+
+}
 
 resource "random_id" "MyRandomNumber" {
   byte_length = 10
@@ -12,16 +14,28 @@ resource "random_id" "MyRandomNumber" {
 
 locals {
   ec2name = "Angad_ec2_instance_1"
-  ec2des = "Angad Singh's EC2 instance, created using Terraform, subnetid-${var.aws_subnet_id}"
+  ec2des  = "Angad Singh's EC2 instance, created using Terraform, subnetid-${var.aws_subnet_id}"
+}
+
+data "aws_ami" "ubuntu_22_04" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
 }
 
 resource "aws_instance" "ec2_instance_1" {
-  ami = "ami-08b5b3a93ed654d19"
+  ami           = data.aws_ami.ubuntu_22_04.id
   instance_type = "t2.micro"
-  subnet_id = var.aws_subnet_id
+  subnet_id     = var.aws_subnet_id
 
   tags = {
-    Name = local.ec2name
+    Name        = local.ec2name
     description = local.ec2des
+    Region      = data.aws_region.MyRegion.name
   }
 }
--- LAB7/variables.tf
+++ LAB7/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_subnet_id" {
   description = "Specify the subnet-id of the instance"
-  type = string
-  default = "subnet-0942424db7b2369f3"
+  type        = string
+  default     = "subnet-0942424db7b2369f3"
 }
