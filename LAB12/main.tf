eaa22d00a5e818994030c73021925e147a00b4c9
AngadSingh04 <singh04angad@gmail.com>
2025-03-15 13:01:05 +0530

    Add demo EC2 instance configuration to LAB12 Terraform setup

--- LAB12/main.tf
+++ LAB12/main.tf
@@ -8,7 +8,19 @@ provider "aws" {
   }
 }
 
-resource "aws_instance" "linux_instance" {
+# resource "aws_instance" "linux_instance" {
+#   instance_type = "t2.micro"
+#   ami           = "ami-08b5b3a93ed654d19"
+# }
+
+resource "aws_instance" "demo_instance" {
+  ami = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
-  ami           = "ami-08b5b3a93ed654d19"
+  subnet_id = "subnet-0942424db7b2369f3"
+  
+
+  tags = {
+    Name = "Angad"
+    description = "This is a demo ec2 instance"
+  }
 }
